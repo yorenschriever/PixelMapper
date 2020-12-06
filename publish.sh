3d8822b07348d70b5943c9871152e28d3282c015
Yoren Schriever <yoren@ocelot.nl>
2020-12-06 17:23:15 +0100

    publish script

---
+++ publish.sh
@@ -0,0 +1,6 @@
+rm -r ../yorenschriever.github.io/pixelmapper
+cp -r build ../yorenschriever.github.io/pixelmapper
+cd ../yorenschriever.github.io/pixelmapper
+git add *
+git commit -m "publish pixelampper"
+git push
