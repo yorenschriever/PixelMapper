231ed291fba4a8f79c11c627c0c1faf3936e709c
Yoren Schriever <yoren@ocelot.nl>
2020-12-06 17:07:44 +0100

    fix concept

--- src/concept/concept.tsx
+++ src/concept/concept.tsx
@@ -29,7 +29,7 @@ export const Concept = () => {
     const stateToVal = (state:boolean|undefined) => state===undefined?"":state?"1":"0"
     const valToState = (val:string) => val==="1"?true:val==="0"?false:undefined
 
-    const solvedVal = (state0?2:0)+(state1?1:0)
+    const solvedVal = (state1?2:0)+(state0?1:0)
     const solvedImg = [solve0,solve1,solve2,solve3]
 
     return <>
@@ -58,39 +58,39 @@ export const Concept = () => {
 
     <img src={white} alt="white"/>
     <span> </span>
-    <img src={code1} alt="stack1"/>
+    <img src={code1} alt="code1"/>
     <span> </span>
-    <img src={code0} alt="stack2"/>
+    <img src={code0} alt="code0"/>
     <span> </span>
     <img src={result} alt="result"/>
 
     <span></span>
     <span></span>
     <span>
-        code bit 1 = <input value={stateToVal(state0)} onChange={e=>setState0(valToState(e.currentTarget.value.slice(-1)))} style={inputCss}/><br/>
-        ({state0?"positive":"negative"})
-    </span>
-    <span></span>
-    <span>
-        code bit 0 = <input value={stateToVal(state1)} onChange={e=>setState1(valToState(e.currentTarget.value.slice(-1)))} style={inputCss}/><br/>
+        code bit 1 = <input value={stateToVal(state1)} onChange={e=>setState1(valToState(e.currentTarget.value.slice(-1)))} style={inputCss}/><br/>
         ({state1?"positive":"negative"})
     </span>
     <span></span>
-    <span>code = B{stateToVal(state0)}{stateToVal(state1)} = <input 
+    <span>
+        code bit 0 = <input value={stateToVal(state0)} onChange={e=>setState0(valToState(e.currentTarget.value.slice(-1)))} style={inputCss}/><br/>
+        ({state0?"positive":"negative"})
+    </span>
+    <span></span>
+    <span>code = B{state1?"1":"0"}{state0?"1":"0"} = <input 
         value={solvedVal} 
         onChange={e=>{
             const val = parseInt(e.currentTarget.value.slice(-1))
-            setState0(val===2 || val===3)
-            setState1(val===1 || val===3)
+            setState1(val===2 || val===3)
+            setState0(val===1 || val===3)
         }}
         style={inputCss}
     /></span>
 
     <img src={start} alt="white"/>
     <span> X </span>
-    <img src={state0?pos0:neg0} alt="stack1"/>
+    <img src={state1?pos1:neg1} alt="stacked1"/>
     <span> X </span>
-    <img src={state1?pos1:neg1} alt="stack2"/>
+    <img src={state0?pos0:neg0} alt="stacked0"/>
     <span> = </span>
     <img src={solvedImg[solvedVal]} alt="result"/>
     
