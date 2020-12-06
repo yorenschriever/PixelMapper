cd4fdf54aa840823162ded50155494c2dbf8fa07
Yoren Schriever <yoren@ocelot.nl>
2020-12-06 17:30:12 +0100

    typo fix

--- publish.sh
+++ publish.sh
@@ -2,5 +2,5 @@ rm -r ../yorenschriever.github.io/pixelmapper
 cp -r build ../yorenschriever.github.io/pixelmapper
 cd ../yorenschriever.github.io/pixelmapper
 git add *
-git commit -m "publish pixelampper"
+git commit -m "publish pixelmapper"
 git push
